829903b920773365915d7b95d6979f9e86ea9b6e
unknown <pame5815@L9LP0XY1.RACKSPACE.CORP>
2015-06-18 09:44:18 -0500

    updated code with first and last instead of new and old

--- server.R
+++ server.R
@@ -1,11 +1,11 @@
-CMGR<-function(new,old,n) paste(round((((new/old)^(1/n))-1)*100,digits=2),"%")
+CMGR<-function(last,first,n) paste(round((((last/first)^(1/n))-1)*100,digits=2),"%")
 
 paste("A", 1, "%") 
 shinyServer(
   function(input,output) {
-    output$inputNew <- renderPrint({input$new})
-    output$inputOld <- renderPrint({input$old})
+    output$inputLast <- renderPrint({input$last})
+    output$inputFirst <- renderPrint({input$first})
     output$inputN <- renderPrint({input$n})
-    output$prediction <- renderPrint({CMGR(input$new,input$old,input$n)})
+    output$prediction <- renderPrint({CMGR(input$last,input$first,input$n)})
   }
 )
--- ui.R
+++ ui.R
@@ -3,13 +3,14 @@ shinyUI(pageWithSidebar(
   #Application title
   headerPanel("Compound Monthly Growth Rate (CMGR)"),
   sidebarPanel(
-    numericInput('old','Old',0),
-    numericInput('new','New',0),
-    numericInput('n','Number of Occurances',0),
+    numericInput('first','First',100),
+    numericInput('last','Last',200),
+    numericInput('n','Number of Occurances',4),
     submitButton('Submit')
   ),
   mainPanel(
-    h2("CMGR Formula: ((New/Old)^(1/N))-1"),
+      wellPanel(p("This is meant to calculate the Compound Monthly Growth Rate.  To the left, insert the first value of your data, then the last value of your data, and finally insert the number of total values that exist in the data. For example, I want to calculate the CMGR of invoiced revenue for an account over 4 months.  The first invoice for this account is $100 in month 1, the last invoice for this account is $200 in month 4.  There are 4 total months, so the CMGR is 18.92%.")),
+    h2("CMGR Formula: ((Last/First)^(1/N))-1"),
 ##    h4('Old Value'),
 ##    verbatimTextOutput("inputOld"),
 ##    h4('New Value'),
